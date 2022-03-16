3c98572676670ab3339ca875ceeea91cc707f6b2
Tina Pham <68769196+phamdongoctram@users.noreply.github.com>
2022-03-15 22:10:14 -0400

    Update task2.py

--- Hackathon Final Exam/task2.py
+++ Hackathon Final Exam/task2.py
@@ -4,13 +4,18 @@ a = int(input("Input a: "))
 b = int(input("Input b: "))
 c = int(input("Input c: "))
 
-discriminant = b**2 - 4 * a * c
-sqrt_dis = math.sqrt(abs(discriminant))
-
-if discriminant > 0:
-    print("The equation has 2 solutions.")
-    print(f'x = {(-b + sqrt_dis) / (2 * a)} or x = {(-b - sqrt_dis) / (2 * a)}')
-elif discriminant == 0:
-    print(f'x = {-b / (2 * a)}')
+if a == 0:
+    print("x =", -c/b)
 else:
-    print("The equation doesn't have any real roots.")
+    discriminant = b**2 - 4 * a * c
+    sqrt_dis = math.sqrt(abs(discriminant))
+
+    if discriminant > 0:
+        print("The equation has 2 solutions.")
+        print(f'x = {(-b + sqrt_dis) / (2 * a)} or x = {(-b - sqrt_dis) / (2 * a)}')
+    elif discriminant == 0:
+        print(f'x = {-b / (2 * a)}')
+    elif b == 0 and c < 0:
+        print("x =", math.sqrt(c), "or", "x =", (-math.sqrt(c)))
+    else:
+        print("The equation doesn't have any real roots.")
